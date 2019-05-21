802cf069df9b85f5b327cafe7f8712474d3b1c53
gonzaloguiher <zalo.agui1@gmail.com>
2019-05-21 13:59:47 +0200

    Añadidos algunos tests

---
+++ Acme-Bet/src/test/java/services/SponsorshipServiceTest.java
@@ -0,0 +1,15 @@
+package services;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/junit.xml"})
+@Transactional
+public class SponsorshipServiceTest extends AbstractTest {
+
+}
